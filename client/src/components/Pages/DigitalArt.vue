5acc1b0dac51e25721e2c89c47dbc50fb899b533
Tamas Zentai <zentaitamas@gmail.com>
2021-02-03 15:23:14 +0000

    gallery lightbox in digitalart gallery is working

--- client/src/components/Pages/DigitalArt.vue
+++ client/src/components/Pages/DigitalArt.vue
@@ -1,18 +1,41 @@
 <template>
   <content>
     <gallery-grid>
-      <div class="galleryCard" v-for="image in getImages" :key="image.id">
-        <img :src="image.url" />{{ image.title }}
+      <div class="galleryCard" v-for="(image, index) in getImages" :key="image.id">
+        <img :src="image.url" @click="openImage(image.url, index)"/>{{ image.title }}
       </div>
     </gallery-grid>
+    <div v-if="isLightboxActive" class="lightbox" @click="closeImage">
+      <img :src="openedImageUrl" class="openedImage">
+    </div>
   </content>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      activeClass: 'lightbox.active',
+      isLightboxActive: false,
+      tempImageUrl: '',
+    }
+  },
+  methods: {
+    openImage(url, index) {
+      this.isLightboxActive = true;
+      this.tempImageUrl = url;
+      console.log(index);
+    },
+    closeImage() {
+      this.isLightboxActive = false;
+    }
+  },
   computed: {
     getImages() {
       return this.$store.getters["digitalArt/getImages"];
+    },
+    openedImageUrl() {
+      return this.tempImageUrl;
     },
   },
 };
@@ -27,6 +50,7 @@ content {
   width: 250px;
   height: 250px;
   text-align: center;
+  
 }
 
 .galleryCard img {
@@ -34,5 +58,24 @@ content {
   width: auto;
   display: block;
   margin: auto;
+  cursor: pointer;
+}
+
+.lightbox {
+  position: fixed;
+  display: flex;
+  z-index: 1000;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.3);
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  color: white;
+}
+
+.openedImage {
+  height: 75vh;
 }
 </style>
