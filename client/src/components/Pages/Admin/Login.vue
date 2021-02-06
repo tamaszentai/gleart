890c0def5f1fe6fece9848e1af53610f5ec5e969
Tamas Zentai <zentaitamas@gmail.com>
2021-02-06 14:56:58 +0000

    login form added

--- client/src/components/Pages/Admin/Login.vue
+++ client/src/components/Pages/Admin/Login.vue
@@ -1,6 +1,13 @@
 <template>
   <section>
     <h2>Login</h2>
+    <form>
+      <label>Username: </label>
+      <input type="text" v-model="userNameInput"/>
+      <label>Password: </label>
+      <input type="password" v-model="passwordInput"/>
+      <button>Login</button>
+    </form>
   </section>
 </template>
 
@@ -8,14 +15,19 @@
   export default {
     data() {
       return {
+        userNameInput: '',
+        passwordInput: ''
       }
     }
   }
 </script>
 
 <style scoped>
+section {
+  color: whitesmoke
+}
 h2 {
   text-align: center;
-  color: white;
+  color: whitesmoke;
 }
 </style>
