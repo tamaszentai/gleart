59d05b0d00d946ff8cbdb43fb20ddbd0f7c64c54
Tamas Zentai <zentaitamas@gmail.com>
2021-02-06 15:07:31 +0000

    updated login

--- client/src/components/Pages/Admin/Login.vue
+++ client/src/components/Pages/Admin/Login.vue
@@ -1,12 +1,12 @@
 <template>
   <section>
     <h2>Login</h2>
-    <form>
+    <form @submit.prevent="login">
       <label>Username: </label>
-      <input type="text" v-model="userNameInput"/>
+      <input type="text" v-model="userNameInput" required/>
       <label>Password: </label>
-      <input type="password" v-model="passwordInput"/>
-      <button>Login</button>
+      <input type="password" v-model="passwordInput" required/>
+      <button type="submit">Login</button>
     </form>
   </section>
 </template>
@@ -17,6 +17,13 @@
       return {
         userNameInput: '',
         passwordInput: ''
+      }
+    },
+    methods: {
+      login() {
+        if (this.userNameInput !== '' & this.passwordInput !== '') {
+          console.log(this.userNameInput, this.passwordInput);
+        }
       }
     }
   }
@@ -29,5 +36,20 @@ section {
 h2 {
   text-align: center;
   color: whitesmoke;
+}
+
+form {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 2rem;
+  background-color: transparent;
+  border: solid whitesmoke 1px;
+}
+
+label {
+  font-weight: bold;
+  color: whitesmoke
 }
 </style>
